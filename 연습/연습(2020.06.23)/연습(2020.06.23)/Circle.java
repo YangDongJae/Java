50f7ade074f3485e0e6d32ec92179a0065c66845
YangDongJae <ydj9805@me.com>
2020-06-24 00:25:19 +0900

    add practice with interface , absract class , abstract method , downcasting

---
+++ 연습/연습(2020.06.23)/연습(2020.06.23)/Circle.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Circle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Circle extends GeometricalFigure 
+{
+    public void draw(){
+        System.out.println("Circle");
+    }
+    
+        public void sendCall(){
+        System.out.println("send call");
+        
+    }
+    
+    public void receiveCall(){
+        System.out.println("receive call");
+    }
+}
---
+++ 연습/연습(2020.06.23)/연습(2020.06.23)/Rectangle.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Triangle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rectangle extends GeometricalFigure 
+{
+    public void draw(){
+        System.out.println("Rectangle");
+    }
+
+    public void sendCall(){
+        System.out.println("send call");
+
+    }
+
+    public void receiveCall(){
+        System.out.println("receive call");
+    }
+}
